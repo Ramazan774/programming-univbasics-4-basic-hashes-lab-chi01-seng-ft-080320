7498d2ef6c8e162af81b68627f2c2abbe7b9b390
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-19 06:09:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,10 +3,7 @@ def new_hash
 end
 
 def my_hash
- my_hash {
-   :name =>"Ramazan",
-   :age=> 26
- } # return a valid hash with any key/value pair of your choice
+ { :name =>"Ramazan"} # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
